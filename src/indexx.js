bd3e0eaa4f2c229c6920b2a640ea399d046c1ae5
Dipesh Devkota <devkotadipesh12@gmail.com>
2024-06-22 23:21:33 +0545

    first commit

--- src/indexx.js
+++ src/indexx.js
@@ -8,12 +8,9 @@ import connectDB from '/src/index.js';
 
 // Attempt to connect to the database
 connectDB();
-.then(()=>{
+
     app.listen(process.env.PORT || 8000,()=>
     {
         console.log(`Sever is runnng at port:${process.env.PORT}`)
     })
-})
-.catch((err)=>{
-    console.log("MONGODB connection failed!!!",err)
-})
+
